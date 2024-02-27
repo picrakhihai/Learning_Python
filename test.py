e59eaccb3dd911052ede5f84fead78ffb3058414
picrakhihai <btech60301.21@bitmesra.ac.in>
2024-02-27 23:17:52 +0530

    Fix formatting in test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 print('the big black box')
 
 print("the\nbig\nblack\nbox")
+
 print("the", end=' ')
 print("big", end=' ')
 print("black", end=' ')
 print("box", end=' ')
 
-print("the
+print("""the
       big
       black
-      box")     #Welcome to first error 😁
+      box""")
