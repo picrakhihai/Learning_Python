13c2a88c2c23294231a668f19ce8478452a98a1d
picrakhihai <btech60301.21@bitmesra.ac.in>
2024-02-27 23:13:51 +0530

    Add print statements to test.py and remove test_newLine.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print('the big black box')
+
+print("the\nbig\nblack\nbox")
+print("the", end=' ')
+print("big", end=' ')
+print("black", end=' ')
+print("box", end=' ')
+
+print("the
+      big
+      black
+      box")     #Welcome to first error 😁
