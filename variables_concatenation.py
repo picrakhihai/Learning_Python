f3499ea48430bdcd8d0cb6b301e2b5ccff886661
picrakhihai <btech60301.21@bitmesra.ac.in>
2024-03-01 00:15:37 +0530

    Add character conversation in variables_concatenation.py

--- variables_concatenation.py
+++ variables_concatenation.py
@@ -9,3 +9,26 @@ print("James: Hi John! How have you been?")
 print("John: I have been well. Do you wanna go get lunch")
 print("James: Yes!")
 print("John: Great, we can meet at In-n-Out burger next week")
+
+character_1 = 'John'
+character_2 = 'Luke'
+print(character_1 + ': Hi ' + character_2 + ', long time since we talked last.')
+print(character_2 + ': Hi ' + character_1 + '! How have you been?')
+print(character_1 + ': I have been well. Do you wanna go get lunch?')
+print(character_2 + ': Yes!')
+print(character_1 + ': Great, we can meet at In-n-Out burger next week')
+
+character_1 = 'James'
+character_2 = 'Luke'
+print(character_1 + ': Hi ' + character_2 + ', long time since we talked last.')
+print(character_2 + ': Hi ' + character_1 + '! How have you been?')
+print(character_1 + ': I have been well. Do you wanna go get lunch?')
+print(character_2 + ': Yes!')
+print(character_1 + ': Great, we can meet at In-n-Out burger next week')
+
+print('one' + 'twenty' + 'one')
+
+value_1 = 'bear'
+value_2 = 'cheetah'
+print(value_1, value_2)
+print(value_1 + value_2)
