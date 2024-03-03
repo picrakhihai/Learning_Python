3b890e0ee9e737500b5a85d020b4e41f1e3bc2a4
picrakhihai <btech60301.21@bitmesra.ac.in>
2024-03-03 23:26:34 +0530

    Add input prompts and conversation between characters

--- 3.ser_input.py
+++ 3.ser_input.py
@@ -2,3 +2,11 @@ input('type an address:') # This line takes input from the user but it has no pl
 
 variable = input('type an address:')
 print("The address you typed is :"+ variable)
+
+character_1 = input("Name of character 1: ")
+character_2 = input("Name of character 2: ")
+print(character_1, ": Hi " + character_2 + ", long time since we talked last." )
+print(character_2, ": Hi " + character_1 + "! How have you been?")
+print(character_1, ": I have been well. Do you wanna go get lunch")
+print(character_2, ": Yes!")
+print(character_1, ": Great, we can meet at In-n-Out burger next week")
