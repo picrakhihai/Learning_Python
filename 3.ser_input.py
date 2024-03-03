212354ec587a000ade2e74ff062d0bd8a26a8998
picrakhihai <btech60301.21@bitmesra.ac.in>
2024-03-03 20:18:27 +0530

    Add variable to store user input address

--- 3.ser_input.py
+++ 3.ser_input.py
@@ -1,1 +1,4 @@
 input('type an address:') # This line takes input from the user but it has no place to return the value i.e. the value can't be stored in any variable.
+
+variable = input('type an address:')
+print("The address you typed is :", variable)
